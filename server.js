d0bff8b3dc3826db5a7a806e5df918b10bdce380
romyjane <23-63809@g.batstate-u.edu.ph>
2025-11-22 07:05:42 +0000

    Update message

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Have a great Day!!!");
-  res.write("Welcome to my affordable Hair and Make up.");
+
+  res.write("==GLAM AND GLOW Studio==\n");
+  res.write("Your Trusted hair and make up services.");
   res.end();
+  
+  res.write("Welcome to my affordable Hair and Makeup services.\n")
 });
 
 server.listen(PORT, () => {
